eb60f3f60d1f6599c19917361c66f5b28c395cdc
salquier <simonalquier92@gmail.com>
2016-11-21 14:55:22 +0100

    Initial commit

---
+++ main/java/com/serli/myhealthpartner/MainActivity.java
@@ -0,0 +1,30 @@
+package com.serli.myhealthpartner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO : Add implementation when the ProfileActivity was implemented.
+        return super.onOptionsItemSelected(item);
+    }
+}
