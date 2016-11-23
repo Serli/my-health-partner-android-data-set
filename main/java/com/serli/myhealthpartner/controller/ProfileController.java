91e5adf7677dc31126a8309b88402a05b052974e
salquier <simonalquier92@gmail.com>
2016-11-23 14:08:30 +0100

    Implementation of the profile controller

--- main/java/com/serli/myhealthpartner/controller/ProfileController.java
+++ main/java/com/serli/myhealthpartner/controller/ProfileController.java
@@ -1,22 +1,37 @@
 package com.serli.myhealthpartner.controller;
 
+import android.content.Context;
+
+import com.serli.myhealthpartner.model.ProfileDAO;
 import com.serli.myhealthpartner.model.ProfileData;
 
 public class ProfileController {
 
+    private Context context;
+
+    private ProfileDAO dao;
+
     private ProfileData profile;
 
-    public ProfileController() {
-        // TODO
+    public ProfileController(Context context) {
+        this.context = context;
+
+        dao = new ProfileDAO(context);
+
+        dao.open();
+        profile = dao.getProfile();
+        dao.close();
     }
 
     public ProfileData getProfile() {
-        // TODO
-        return null;
+        return profile;
     }
 
     public void setProfile(ProfileData data) {
-
+        dao.open();
+        dao.addEntry(data);
+        dao.close();
     }
+
 
 }
