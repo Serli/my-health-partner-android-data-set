be75a945b3959d5fbe83d7c1ae02d5eedc2085a2
email <oubaziz.k92@gmail.com>
2016-11-24 10:57:23 +0100

    public void addEntry(float x, float y, float z, long timestamp) public List<AccelerometerData> getDatas() public void deleteDatas() private AccelerometerData cursorData(Cursor cursor)

--- main/java/com/serli/myhealthpartner/model/AccelerometerDAO.java
+++ main/java/com/serli/myhealthpartner/model/AccelerometerDAO.java
@@ -1,16 +1,18 @@
 package com.serli.myhealthpartner.model;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccelerometerDAO {
 
     private Database database;
     private SQLiteDatabase db;
-    private String[] allColumns = { /* TODO  */};
+    private String[] allColumns = { database.ACC_TSTMP, database.ACC_X, database.ACC_Y, database.ACC_Z};
 
     public AccelerometerDAO(Context context) {
         this.database = new Database(context);
@@ -25,20 +27,44 @@ public class AccelerometerDAO {
     }
 
     public void addEntry(float x, float y, float z, long timestamp) {
-        // TODO
+
+        ContentValues values = new ContentValues();
+        values.put(database.ACC_X, x);
+        values.put(database.ACC_Y, y);
+        values.put(database.ACC_Z, z);
+        values.put(database.ACC_TSTMP, timestamp);
+
+
+        db.insert(database.ACC_TABLE, null, values);
+
     }
 
     public List<AccelerometerData> getDatas() {
-        // TODO
-        return null;
+
+        List<AccelerometerData> acce_data_list = new ArrayList<AccelerometerData>();
+        Cursor cursor = db.query(database.ACC_TABLE,
+                allColumns, null, null, null, null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            AccelerometerData acce_data = cursoToData(cursor);
+            acce_data_list.add(acce_data);
+            cursor.moveToNext();
+        }
+        return acce_data_list;
     }
 
     public void deleteDatas() {
-        // TODO
+
+        db.delete(database.ACC_TABLE, null , null);
     }
 
     private AccelerometerData cursoToData(Cursor cursor) {
-        // TODO
-        return null;
+        AccelerometerData acce_data = new AccelerometerData();
+        acce_data.setTimestamp(cursor.getInt(1));
+        acce_data.setX(cursor.getFloat(2));
+        acce_data.setY(cursor.getFloat(3));
+        acce_data.setZ(cursor.getFloat(4));
+
+        return acce_data;
     }
 }
