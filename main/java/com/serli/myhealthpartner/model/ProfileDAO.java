2d11fdf8bff027dde08a07f53587466e1defab80
salquier <simonalquier92@gmail.com>
2016-11-24 11:14:01 +0100

    Add comment in ProfileDOA + empty the profile table before put a new entry inside

--- main/java/com/serli/myhealthpartner/model/ProfileDAO.java
+++ main/java/com/serli/myhealthpartner/model/ProfileDAO.java
@@ -5,46 +5,70 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Link between the profile table in the {@link Database} and the controllers.
+ */
 public class ProfileDAO {
 
     private Database database;
     private SQLiteDatabase db;
     private String[] allColumns = {database.PROFILE_ID, database.PROFILE_HEIGHT, database.PROFILE_WEIGHT, database.PROFILE_AGE, database.PROFILE_GENDER};
 
+    /**
+     * @param context The context where the dao is called.
+     */
     public ProfileDAO(Context context) {
         this.database = new Database(context);
     }
 
+    /**
+     * Open the connection with the database.
+     */
     public void open() {
         db = database.getWritableDatabase();
     }
 
+    /**
+     * Close the connection with the database.
+     */
     public void close() {
         database.close();
     }
 
+    /**
+     * Add an entry in the profile table.<br/>
+     * As only one profile is needed in the application, the table will be empty before the insertion.
+     *
+     * @param data The data object containing the profile information
+     */
     public void addEntry(ProfileData data) {
+        if (data.getBirthday() != null && data.getSize() != 0 && data.getWeight() != 0) {
+            db.delete(Database.PROFILE_TABLE, null, null);
 
-        ContentValues values = new ContentValues();
-        values.put(database.PROFILE_HEIGHT, data.getSize());
-        values.put(database.PROFILE_WEIGHT, data.getWeight());
-        values.put(database.PROFILE_AGE, data.getBirthday().getTime());
-        values.put(database.PROFILE_GENDER, data.getSex());
+            ContentValues values = new ContentValues();
+            values.put(Database.PROFILE_HEIGHT, data.getSize());
+            values.put(Database.PROFILE_WEIGHT, data.getWeight());
+            values.put(Database.PROFILE_AGE, data.getBirthday().getTime());
+            values.put(Database.PROFILE_GENDER, data.getSex());
 
-        db.insert(database.PROFILE_TABLE, null, values);
+            db.insert(Database.PROFILE_TABLE, null, values);
+        }
     }
 
+    /**
+     * Return a {@link ProfileData} object containing the profile stored.
+     *
+     * @return The profile.
+     */
     public ProfileData getProfile() {
-        Cursor c = db.query(database.PROFILE_TABLE, allColumns, null, null, null, null, null);
+        Cursor c = db.query(Database.PROFILE_TABLE, allColumns, null, null, null, null, null);
+        c.moveToFirst();
         return cursorToData(c);
     }
 
     private ProfileData cursorToData(Cursor cursor) {
-        cursor.moveToFirst();
-
         ProfileData prof_data = new ProfileData();
         prof_data.setSize(cursor.getInt(1));
         prof_data.setWeight(cursor.getInt(2));
